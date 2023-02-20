7bb6ccced5464aa403e77c032b89e9bf18c51273
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-02-20 14:53:58 +0200

    First commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,17 @@
+'use client'
+import { useEffect, useState } from 'react'
 import './globals.css'
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    const threeScript = document.createElement('script')
+    threeScript.setAttribute('id','threeScript')
+    threeScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js')
+    document.getElementsByTagName('head')[0].appendChild(threeScript)
+    return () => {
+      if (threeScript) threeScript.remove();
+    }
+  }, [])
   return (
     <html lang="en">
       {/*
@@ -8,7 +19,9 @@ export default function RootLayout({ children }) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   )
 }
---
+++ app/page.jsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useEffect, useRef, useState } from "react"
+import NET from 'vanta/dist/vanta.net.min'
+import * as THREE from 'three'
+
+export default function Home() {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        NET({
+          el: vantaRef.current,
+          THREE,
+          color: 0xff3f81,          
+          backgroundColor: 0x0,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.00,
+          minWidth: 200.00,
+          scale: 1.00,
+          scaleMobile: 1.00,
+          backgroundColor: 0x0,
+          points: 20.00,
+          maxDistance: 21.00,
+          spacing: 12.00
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destory();
+    };
+  }, [vantaEffect]);
+  
+  return (
+    <div className='main' ref={vantaRef}>hello</div>
+  )
+}
