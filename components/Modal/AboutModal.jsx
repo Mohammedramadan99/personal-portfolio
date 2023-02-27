99cb7ec0607d9b47c3c9a44a6e739de6df8cf8a1
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-02-27 23:50:20 +0200

    Delete single qoute from AboutModal

--- components/Modal/AboutModal.jsx
+++ components/Modal/AboutModal.jsx
@@ -28,7 +28,7 @@ function AboutModal({openModal,setOpenModal}) {
                 ></div>
             </div>
             <div className="py-5text-sm font-semibold leading-7 text-gray-400 selection:bg-main_color selection:text-gray-200">
-              I am a Front End Developer who enjoys working in a team that supports continuous learning and strives for high-quality work. I am constantly working to improve my skills. building dynamic web projects and Developing UI design I'm focusing on implementing clean code and easy to read at a glance. So I'm always seeking to have an opportunity that matches my skills, to make the best use of all what I have learned.
+              I am a Front End Developer who enjoys working in a team that supports continuous learning and strives for high-quality work. I am constantly working to improve my skills. building dynamic web projects and Developing UI design I am focusing on implementing clean code and easy to read at a glance. So I am always seeking to have an opportunity that matches my skills, to make the best use of all what I have learned.
             </div>
         </div>
         {/* bottom */}
