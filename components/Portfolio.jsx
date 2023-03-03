39b2406d0c0980a01c68badc7031f1d0a2d21ae2
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-03-03 12:58:59 +0200

    Update ProjectDetails component

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -19,17 +19,80 @@ import {motion} from 'framer-motion'
 import { CursorContext } from "@/context/CursorContext";
 import { transition1 } from "@/utils/transitions";
 import Link from "next/link";
+import ProjectDetails from "./ProjectDetails";
 
 function Portfolio() {
   const {mouseEnterHandler,mouseLeaveHandler} = useContext(CursorContext)
+  const projects = [
+    {
+      title:"e-commerce",
+      img:ecommerce_img,
+      description:"lorem",
+      category:"full-stack project",
+      fromColor:"from-orange-400",
+      toColor:"to-orange-500",
+      color:"text-orange-600",
+      bg:"bg-orange-600/10",
+      rotate:"img_container_rotate1"
+    },
+    {
+      title:"social media",
+      img:sm_img,
+      description:"lorem",
+      category:"full-stack project",
+      fromColor:"from-yellow-400",
+      toColor:"to-yellow-500",
+      color:"text-yellow-600",
+      bg:"bg-yellow-600/10",
+      rotate:"img_container_rotate3"
+    },
+    {
+      title:"blog",
+      img:blog_img,
+      description:"lorem",
+      category:"full-stack project",
+      fromColor:"from-green-400",
+      toColor:"to-green-500",
+      color:"text-green-600",
+      bg:"bg-green-600/10",
+      rotate:"img_container_rotate2"
+    },
+    {
+      title:"moz portfolio 1",
+      img:portfolio_img,
+      description:"lorem",
+      category:"full-stack project",
+      fromColor:"from-rose-400",
+      toColor:"to-rose-500",
+      color:"text-rose-600",
+      bg:"bg-rose-600/10",
+      rotate:"img_container_rotate3"
+    },
+    {
+      title:"moz portfolio 2",
+      img:portfolio_img,
+      description:"lorem",
+      category:"full-stack project",
+      fromColor:"from-sky-400",
+      toColor:"to-sky-500",
+      color:"text-sky-600",
+      bg:"bg-sky-600/10",
+      rotate:"img_container_rotate2"
+    },
+  ]
 
-  const [hoverItem,setHoverItem] = useState(false)
+  const [showDetails,setShowDetails] = useState(false)
+  const [projectDetails,setProjectDetails] = useState({})
+  const showDetailsHandler = project => {
+    setShowDetails(true)
+    setProjectDetails(project)
+  }
   return (
-    <div className='animation-fadeUp w-screen  h-4/5 flex-center '>
+    <div className='animation-fadeUp w-screen relative  h-4/5 flex-center'>
       <div className="flex flex-col w-screen md:w-[90%] lg:w-4/5 gap-x-4 h-96">
         <h1 className='text-3xl font-extrabold capitalize'>creative <span className='text-main_color'>portfolio</span></h1>
         <div className="mt-20">
-        <Swiper 
+        <Swiper
                 // slidesPerView={3}
                 spaceBetween={30}
                 slidesPerView={1}
@@ -54,15 +117,16 @@ function Portfolio() {
             >
                 {projects?.map(project => (
                     <SwiperSlide key={project.title} className="overflow-hidden rounded-xl bg-transparent">
-                      <motion.Link
-                      href={`/projectDetails/${project.id}`}
+                      <motion.div
                       initial={{opacity:0,scale:0}}
                       animate={{opacity:1,scale:1}}
                       exit={{opacity:0,scale:.1}}
                       transition={transition1}
                       onMouseEnter={mouseEnterHandler}
                       onMouseLeave={mouseLeaveHandler}
-                      className={`group relative w-full h-full p-10 ${project.bg} backdrop-blur-lg `}>
+                      className={`group w-full h-full p-10 ${project.bg} backdrop-blur-lg `}
+                      onClick={() => showDetailsHandler(project)}
+                      >
                         {/* bg-gradient-to-l from-${project.color}-500 to-${project.color}-600 */}
                         <div className={`img_container ${project.rotate} relative w-full h-full m-auto duration-700 ease-in-out rounded-3xl  shadow-2xl shadow-black/50 before:absolute before:z-10 before:rounded-xl before:blur-xl`}>
                           <Image 
@@ -75,10 +139,11 @@ function Portfolio() {
                           <div className="">{project.category}</div>
                           <div className={`${project.color}`}>{project.title}</div>
                         </div>
-                      </motion.Link>
+                      </motion.div>
                     </SwiperSlide>
                 ))}
             </Swiper>
+            {showDetails && <ProjectDetails/> }
         </div>
       </div>
     </div>
--- components/ProjectDetails.jsx
+++ components/ProjectDetails.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
-function ProjectDetails() {
+import { transition1 } from '@/utils/transitions'
+import {motion} from 'framer-motion'
+function ProjectDetails({projectDetails}) {
   return (
-    <div>ProjectDetails</div>
+    <motion.div
+      initial={{opacity:0,scale:0}}
+      animate={{opacity:1,scale:1}}
+      exit={{opacity:0,scale:.1}}
+      transition={transition1}
+      // onMouseEnter={mouseEnterHandler}
+      // onMouseLeave={mouseLeaveHandler}
+      className={`absolute w-full h-full p-10 top-0 left-0 backdrop-blur-lg bg-neutral-800/80 z-50`}>
+        {projectDetails?.description}
+    </motion.div>
   )
 }
 
